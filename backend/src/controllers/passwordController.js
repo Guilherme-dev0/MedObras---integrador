9a4c8c99e926d9d8429edaa1122c5555f574f2c1
Guilherme <guilherme@example.com>
2026-01-30 20:03:54 -0300

    Fix: Remove bcrypt nativo e simplifica postinstall para corrigir build na Vercel

--- backend/src/controllers/passwordController.js
+++ backend/src/controllers/passwordController.js
@@ -1,5 +1,5 @@
 import prisma from "../db/prisma.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { gerarTokenRecuperacao, hashToken } from "../utils/token.js";
 
 export const forgotPassword = async (req, res) => {
