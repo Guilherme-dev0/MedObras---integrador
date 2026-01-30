bf0696dd8fd9c4cce5e40ec43f603b43f473af3e
Guilherme <guilherme@example.com>
2026-01-30 16:25:19 -0300

    fix: correção de case-sensitive no import do ProtectedRoute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ import Enderecos from "./pages/enderecos.jsx";
 import Medicoes from "./pages/medicoes.jsx";
 import EnderecosListar from "./pages/enderecosListar.jsx";
 import EnderecoEditar from "./pages/enderecoEditar.jsx";
-import ProtectedRoutes from "./componentes/ProtectedRoutes.jsx";
+import ProtectedRoute from "./componentes/protectedRoutes";
 import EditarMedicao from "./pages/editarMedicao.jsx";
 import MedicoesConcluidas from "./pages/medicoesConcluidas.jsx";
 import AgendarMedicao from "./pages/agendarMedicao.jsx";
@@ -37,7 +37,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/resetar-senha" element={<ResetarSenha />} />
 
         {/* ROTAS PROTEGIDAS */}
-        <Route element={<ProtectedRoutes />}>
+        <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
            <Route path="/medicoes/editar/:id" element={<EditarMedicao />} />
            <Route path="/medicoes/concluidas" element={<MedicoesConcluidas />} />
